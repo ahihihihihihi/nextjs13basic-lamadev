3f1b5c17452af86b187cd9921a6438a0030eb1ac
ahihi <bdsthocu20@gmail.com>
2023-12-12 17:38:43 +0700

    #15

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,19 +1,71 @@
 "use client";
 
-import { signIn, } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import styles from "./login.module.css";
+import { useState } from "react";
+import Link from "next/link";
 
 const Login = () => {
+
+    const session = useSession();
+    console.log(">>>check session: ", session)
+
+    const [error, setError] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const email = e.target[0].value;
+        const password = e.target[1].value;
+
+        signIn("credentials", {
+            email,
+            password,
+        });
+    };
+
     return (
-        <div>
+        <div className={styles.container}>
+            <h2 className={styles.subtitle}>Please sign in to see the dashboard.</h2>
+
+            <form onSubmit={handleSubmit} className={styles.form}>
+                <input
+                    type="text"
+                    placeholder="Email"
+                    required
+                    className={styles.input}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    required
+                    className={styles.input}
+                />
+                <button className={styles.button}>Login</button>
+                {error && error}
+            </form>
+            <span className={styles.or}>- OR -</span>
             <button
                 onClick={() => {
                     signIn("google");
                 }}
+                className={styles.button + " " + styles.google}
             >
                 Login with Google
             </button>
+            <button
+                onClick={() => {
+                    signIn("github");
+                }}
+                className={styles.button + " " + styles.github}
+            >
+                Login with Github
+            </button>
+            <span className={styles.or}>- OR -</span>
+            <Link className={styles.link} href="/dashboard/register">
+                Create new account
+            </Link>
         </div>
-    )
+    );
 }
 
 export default Login;
